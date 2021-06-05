45b6e39aae662c590289dfaf584f5478c585b3e9
priyanshsingh <priyanshsingh1401@gmail.com>
2021-06-05 16:19:58 +0530

    added global.js

---
+++ global.js
@@ -0,0 +1,15 @@
+//Globla Object is already embedded in the NODE and we can use its properties directley.
+
+//console.log(global);
+
+// setTimeout(() => {
+//     console.log("Hemlo World!");
+//     clearInterval(int);
+// }, 3000);
+
+// const int = setInterval(() => {
+//     console.log("Batao yaar!");
+// }, 1000);
+
+console.log(__dirname);
+console.log(__filename);
