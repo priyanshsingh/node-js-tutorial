b2f37467d850ae3defaffefc9c03f6d79cdabf38
priyanshsingh <priyanshsingh1401@gmail.com>
2021-06-06 16:03:44 +0530

    modified stream.js

--- stream.js
+++ stream.js
@@ -1,8 +1,14 @@
 const fs = require('fs');
 
-const readStream = fs.createReadStream('./File System Test Files/BlogSt.txt');
+const readStream = fs.createReadStream('./File System Test Files/BlogSt.txt', 'utf-8');
+const writeStream = fs.createWriteStream('./File System Test Files/BlogSt1.txt')
 
-readStream.on('data', (chunk) => {
-    console.log('-----New Chunk-----');
-    console.log(chunk);
-});
+// readStream.on('data', (chunk) => {
+//     console.log('-----New Chunk-----');
+//     console.log(chunk);
+//     writeStream.write('\n NEW CHUNK \n');
+//     writeStream.write(chunk); 
+// }); 
+
+//Piping 
+readStream.pipe(writeStream);
