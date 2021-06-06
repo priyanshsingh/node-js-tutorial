6ddbe84603e660745ac66da37bb272d21af5839c
priyanshsingh <priyanshsingh1401@gmail.com>
2021-06-06 13:46:42 +0530

    added modules and people

---
+++ modules.js
@@ -0,0 +1,3 @@
+const xyz = require('./people');
+
+console.log(xyz.people, xyz.ages);
---
+++ people.js
@@ -0,0 +1,8 @@
+const people = ['Varun', 'Tarun', 'Jack', 'Jill'];
+const ages = [18,19,20,30];
+
+console.log(people);
+
+module.exports = {
+    people, ages
+};
