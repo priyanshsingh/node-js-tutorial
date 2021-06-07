5a3d7b4d0c0b81e73b397a2c7d3e8eb49d8aed82
priyanshsingh <priyanshsingh1401@gmail.com>
2021-06-07 13:17:48 +0530

    added server1.js which responds an html file from the server

--- server1.js
+++ server1.js
@@ -1,13 +1,22 @@
 const http = require('http');
+const fs  = require('fs');
 
 const server = http.createServer((req, res) => {
     console.log(req.url, req.method); 
 
     // Set header content type for the response
     res.setHeader('Content-Type', 'text/html');
-    res.write('<h3>Hemlo Darkness</H3>');
-    res.write('<p>Hemlo Darkness</p>');
-    res.end();
+    
+    // send html file
+    fs.readFile('./views/index.html', (err, data) => {
+        if(err){
+            console.log(err);
+            res.end();
+        }
+        else{
+            res.end(data);
+        }
+    })
 }); 
 
 server.listen(3000, 'localhost', () => {
