6c861a6331cafa9ad59e43808efcd0e1adefac8e
priyanshsingh <priyanshsingh1401@gmail.com>
2021-06-07 12:53:23 +0530

    modified server.js

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const http = require('http');
 []
 const server = http.createServer((req, res) => {
-    console.log('Request made!!!');
+    console.log(req.url, req.method); 
 }); 
 
 server.listen(3000, 'localhost', () => {
     console.log('Listening for requests of Port 3000');
-}); 
+});
