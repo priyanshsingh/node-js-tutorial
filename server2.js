c8fe85ac548e963bb5b1ef3f22a400a054460e48
priyanshsingh <priyanshsingh1401@gmail.com>
2021-06-07 14:18:38 +0530

    added npm module lodash and nodemon

--- server2.js
+++ server2.js
@@ -1,5 +1,6 @@
 const http = require('http');
 const fs  = require('fs');
+const _ = require('lodash');
 
 const server = http.createServer((req, res) => {
     console.log(req.url, req.method); 
@@ -11,12 +12,20 @@ const server = http.createServer((req, res) => {
     switch(req.url){
         case '/':
             path += 'index.html';
+            res.statusCode = 200;
             break;
         case '/about':
             path += 'about.html';
+            res.statusCode = 200;
+            break;
+        case '/about-us':
+            res.statusCode = 301;
+            res.setHeader('Location', './about');
+            res.end();
             break;
         default:
             path += '404.html';
+            res.statusCode = 404;
             break;
     }
     
