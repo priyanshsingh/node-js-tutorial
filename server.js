bec951bad77453598f6ec57cf63a4bcc8d098bbf
priyanshsingh <priyanshsingh1401@gmail.com>
2021-06-07 13:02:11 +0530

    modified server.js

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ const http = require('http');
 []
 const server = http.createServer((req, res) => {
     console.log(req.url, req.method); 
+
+    // Set header content type for the response
+    res.setHeader('Content-Type', 'text/plain');
+    res.write('Hemlo Darkness');
+    res.end();
 }); 
 
 server.listen(3000, 'localhost', () => {
